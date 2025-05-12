6875663f04ba62a93f303ea3b25030ea2e08ce2c
takeshi <takeshi.yamada1010@gmail.com>
2025-05-12 19:49:28 +0900

    meta-tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def default_meta_tags
+  {
+    site: "🍌Banana Fortune",
+    title: "本日のウホ報！",
+    reverse: true,
+    charset: "utf-8",
+    description: "バナナで今日の運勢を占おう！",
+    keywords: "占い, ゴリラ, バナナ, Banana, Fortune, ウホ",
+    og: {
+      site_name: :site,
+      title: :title,
+      description: :description,
+      type: 'website',
+      url: root_url,
+      image: image_url("banana_fortune.jpg"),
+      locale: "ja-JP"
+    },
+    twitter: {
+      card: "summary_large_image",
+      image: image_url("banana_fortune.jpg")
+    }
+  }
+  end
 end
