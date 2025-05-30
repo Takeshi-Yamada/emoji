97123079dc0463d7fd5398fa5adeed151958d25c
takeshi <takeshi.yamada1010@gmail.com>
2025-05-30 16:40:33 +0900

    questionテーブル動作確認完了

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,22 @@
+class QuestionsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def creat
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
