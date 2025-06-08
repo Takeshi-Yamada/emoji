d23928be1af908e6bb11ac0c7fb4c7903a7e77c7
takeshi <takeshi.yamada1010@gmail.com>
2025-06-08 21:41:38 +0900

    編集画面作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   def index
     @questions = Question.all
   end
@@ -10,7 +12,7 @@ class QuestionsController < ApplicationController
   def create
     @question = current_user.questions.build(question_params)
     if @question.save
-      redirect_to question_path, success: '登録ができました'
+      redirect_to question_path(@question), success: '登録ができました'
     else
       flash.now[:error] = '登録に失敗しました'
       render :new, status: :unprocessable_entity
@@ -28,7 +30,7 @@ class QuestionsController < ApplicationController
   def update
     @question = current_user.questions.find(params[:id])
     if @question.update(question_params)
-      redirect_to question_path(@question), success: '更新に成功しました'
+      redirect_to question_path(@question), success: '編集が成功しました'
     else
       flash.now[:danger] = '更新に失敗しました'
       render :edit, status: :unprocessable_entity
@@ -45,5 +47,9 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:content, :correct)
+  end
+
+  def not_found
+    redirect_back(fallback_location: root_path, alert: "対象のクイズが見つからないか、編集権限がありません。")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_08_081602) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_08_105816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,7 +27,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_08_081602) do
 
   create_table "questions", force: :cascade do |t|
     t.string "content", null: false
-    t.string "answer", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
