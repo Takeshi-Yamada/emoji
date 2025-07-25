390cc9b89c3172366a010dd2a49f0955a59fc5cf
takeshi <takeshi.yamada1010@gmail.com>
2025-07-25 15:43:55 +0900

    テスト内容を確認する用のコミット

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,9 +1,11 @@
 FactoryBot.define do
   factory :question do
-    content { '📓🖊️💀😈🍎' }
+    #絵文字はモノによっては文字数がブレるので変更しないこと
+    content { '🧠🚀🎯🐱🍙' }
     correct { 'デスノート' }
     hint_1 { '死神はリンゴが好き' }
     hint_2 { 'ノートに名前を書かれた人は死ぬ' }
     hint_3 { '新世界の神になる' }
+    association :user
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   it '名前・アドレス・パスワードがあること' do
     user = build(:user)
     expect(user).to be_valid
+    expect(user.errors).to be_empty
   end
 
   it 'メールはユニークであること' do
@@ -12,5 +13,19 @@ RSpec.describe User, type: :model do
     user2.email = user1.email
     user2.valid?
     expect(user2.errors[:email]).to include('はすでに存在します')
+  end
+
+  it 'PWは6文字以上であること' do
+    user = build(:user)
+    user.password = 'abc'
+    user.valid?
+    expect(user.errors[:password]).to include('は6文字以上で入力してください')
+  end
+
+  it '名前は50文字以下であること' do
+    user = build(:user)
+    user.name = 'a' * 51
+    user.valid?
+    expect(user.errors[:name]).to include('は50文字以内で入力してください')
   end
 end
