ad9ed2803b7526fbf4ebcd1cd02271600c7ddb29
takeshi <takeshi.yamada1010@gmail.com>
2025-07-25 11:37:03 +0900

    deviseエラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_many :questions
   has_many :answers
--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,14 @@ module Myapp
       g.assets false
       g.skip_routes true
       g.helper false
-      g.test_framework nil
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
     # Configuration for the application, engines, and railties goes here.
     #
