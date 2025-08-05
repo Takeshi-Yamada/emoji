b9f5071d5875b1a4ed9cc9d8f540723fd643e54d
takeshi <takeshi.yamada1010@gmail.com>
2025-08-05 17:16:09 +0900

    マイページ編集バグ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
 
   has_many :questions
   has_many :answers
